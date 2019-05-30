871dad65ea46e85ccc02b3a30ec1e6d606f1e478
mzamoranos <mzamoranos@uoc.edu>
2019-05-30 21:38:01 +0200

    version1.0

---
+++ index.js
@@ -0,0 +1,11 @@
+/*import './style.css,./lenguajes_frontend.html,./module_bundlers.html,./rollupjs.html,./clojurescript.html';*/
+console.log('carga index.js');
+//var req = require('llamadaFuncion()');
+/*document.getElementById("");
+window.myFunction = function() {
+    var name = 'world';
+    console.log('hola ' + name);
+}*/
+function llamadaFuncion() {
+    alert("leyendo llamadaFuncion");
+}
